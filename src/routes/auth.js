3d0352b12e5519772c6895439d7ab4ad8f696b8c
Akshat451-ctrl <akshatghatiya96@gmail.com>
2025-05-31 11:54:08 +0530

    fixed login controller imports

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { signupValidationRules, validateSignup } from '../middlewares/signupValidation.js';
 import { loginValidationRules, validateLogin } from '../middlewares/loginValidation.js';
 import { userSignup, vendorSignup } from '../controllers/signupController.js';
-import { userLogin, vendorLogin } from '../controllers/LoginController.js'; // Import the login controllers
+import { userLogin, vendorLogin } from '../controllers/loginController.js'; // Import the login controllers
+import { LoginController } from '../controllers/loginController.js';
 
 const router = express.Router();
 
